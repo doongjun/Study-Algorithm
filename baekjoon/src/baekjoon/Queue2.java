f3e001bac634978f02e9c479435c46382c334fb0
doongjun <doongjun@kakao.com>
2021-07-10 23:06:30 +0900

    2021.07.10

--- baekjoon/src/baekjoon/Queue2.java
+++ baekjoon/src/baekjoon/Queue2.java
@@ -1,12 +1,41 @@
 package baekjoon;
 
+import java.util.*;
+
 //1158
 //요세푸스 문제
 public class Queue2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		Queue<Integer> queue = new LinkedList<>();
+		
+		int n = scan.nextInt();
+		int k = scan.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			queue.offer(i);
+		}
+		
+		int cnt = 1;
+		System.out.print("<");
+		while(!queue.isEmpty()) {
+			if(cnt!=k) {
+				queue.offer(queue.poll());
+				cnt++;
+				continue;
+			}
+			if(queue.size()==1) {
+				System.out.print(queue.poll());
+			}
+			else {
+				System.out.print(queue.poll()+", ");
+			}
+			
+			cnt=1;
+		}
+		System.out.print(">");
 	}
+	
 
 }
