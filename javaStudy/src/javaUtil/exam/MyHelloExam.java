9b097e9536fb8e0a2d10e598321adc4792f5b7bf
doongjun <doongjun@kakao.com>
2021-07-26 22:07:42 +0900

    2021.07.26

---
+++ javaStudy/src/javaUtil/exam/MyHelloExam.java
@@ -0,0 +1,30 @@
+package javaUtil.exam;
+
+import java.lang.reflect.Method;
+
+public class MyHelloExam {
+
+	public static void main(String[] args) {
+		
+		MyHello hello = new MyHello();
+		
+		try {
+			Method method = hello.getClass().getDeclaredMethod("hello");
+			
+			if(method.isAnnotationPresent(Count100.class)) {
+				for(int i=0; i<100; i++) {
+					hello.hello();
+				}
+				
+			}else {
+				hello.hello();
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
