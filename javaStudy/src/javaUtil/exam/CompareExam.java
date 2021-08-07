09504a1713b0b9edb2265874be912d46d2797478
doongjun <doongjun.kim@gmail.com>
2021-08-07 22:39:53 +0900

    2021.08.07

---
+++ javaStudy/src/javaUtil/exam/CompareExam.java
@@ -0,0 +1,18 @@
+package javaUtil.exam;
+
+public class CompareExam {
+	public static void exec(Compare compare) {
+		int k = 10;
+		int m = 20;
+		int value = compare.CompareTo(k, m);
+		System.out.println(value);
+	}
+	public static void main(String[] args) {
+		
+		exec((i, j)->{
+			return i - j;
+		});
+		
+	}
+
+}
