e4a6152d9a7e04f13b41d1e5365bf5053ced5faa
doongjun <doongjun@kakao.com>
2021-11-12 14:37:46 +0900

    2021.11.12

---
+++ baekjoon/src/baekjoon/Implementation2.java
@@ -0,0 +1,44 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+//2941 크로아티아 알파벳
+public class Implementation2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<word.length(); i++) {
+            stack.push(word.charAt(i));
+        }
+
+        int count = 0;
+        while(!stack.isEmpty()) {
+            char x = stack.pop();
+            if(x == '=') {
+                char y = stack.pop();
+                if(y == 'z') {
+                    if(!stack.isEmpty()) {
+                        if(stack.peek() == 'd') {
+                            stack.pop();
+                        }
+                    }
+                }
+            }else if(x == '-') {
+                stack.pop();
+            }else if(x == 'j') {
+                if(!stack.isEmpty()) {
+                    if(stack.peek() == 'l' || stack.peek() == 'n') {
+                        stack.pop();
+                    }
+                }
+            }
+            count ++;
+        }
+        System.out.println(count);
+    }
+}
---
+++ baekjoon/src/baekjoon/Queue3.java
@@ -0,0 +1,54 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+//1966 프린터 큐
+public class Queue3 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+
+        int T = Integer.parseInt(br.readLine());
+        for(int i=0; i<T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            LinkedList<ArrayList<Integer>> queue = new LinkedList<>();
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<n; j++) {
+                queue.offer(new ArrayList<>(Arrays.asList(j, Integer.parseInt(st.nextToken()))));
+            }
+
+            int count = 0;
+            while(!queue.isEmpty()) {
+                ArrayList<Integer> front = queue.poll();
+                boolean isMax = true;
+                for(int j=0; j<queue.size(); j++) {
+                    if(queue.get(j).get(1) > front.get(1)) {
+                        queue.offer(front);
+                        for(int k=0; k<j; k++) {
+                            queue.offer(queue.poll());
+                        }
+                        isMax = false;
+                        break;
+                    }
+                }
+                if(isMax == false) continue;
+
+                count++;
+                if(front.get(0) == m) {
+                    break;
+                }
+            }
+            sb.append(count).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
