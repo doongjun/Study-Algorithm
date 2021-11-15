719999d2ba465b8d40a1e4e5690855d2786c25a5
doongjun <doongjun.kim@gmail.com>
2021-11-15 11:02:58 +0900

    2021.11.15

---
+++ baekjoon/src/baekjoon/Queue4.java
@@ -0,0 +1,35 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+// 11866 요세푸스문제
+public class Queue4 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=1; i<=n; i++) {
+            queue.offer(i);
+        }
+        StringBuffer sb = new StringBuffer();
+        sb.append("<");
+
+        while(queue.size() > 1) {
+            for(int i=0; i<k-1; i++) {
+                queue.offer(queue.poll());
+            }
+            sb.append(queue.poll()).append(", ");
+        }
+        sb.append(queue.poll()).append(">");
+
+        System.out.println(sb);
+    }
+}
