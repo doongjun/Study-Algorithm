77085f821a65e943a77a2f5b34b557deb4b4a227
김동준 <doongjun@kakao.com>
2021-12-14 09:28:52 +0900

    2021.12.14

---
+++ baekjoon/src/baekjoon/Queue5.java
@@ -0,0 +1,36 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+// 2164 카드2
+public class Queue5 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int i=1; i<n+1; i++) {
+            queue.offer(i);
+        }
+
+        boolean flag = false;
+        while(queue.size() != 1) {
+            if(!flag) {
+                queue.poll();
+                flag = true;
+                continue;
+            }
+            queue.add(queue.poll());
+            flag = false;
+        }
+        System.out.println(queue.poll());
+    }
+}
