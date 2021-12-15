68a17208f79b95d9d9c4e1c4b218c57ea947e6c0
김동준 <doongjun@kakao.com>
2021-12-15 16:05:35 +0900

    2021.12.15

---
+++ baekjoon/src/baekjoon/HashMap1.java
@@ -0,0 +1,34 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+// 10816 숫자 카드2
+public class HashMap1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuffer sb = new StringBuffer();
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++) {
+            int key = Integer.parseInt(st.nextToken());
+            map.put(key, map.getOrDefault(key, 0)+1);
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<m; i++) {
+            int key = Integer.parseInt(st.nextToken());
+            sb.append(map.getOrDefault(key, 0)).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
