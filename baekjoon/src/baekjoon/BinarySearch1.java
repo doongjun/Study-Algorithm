3b5f5b4da5c4aba79e7e7fd4ba7eb6bd5f9d8d52
doongjun <doongjun@kakao.com>
2022-01-21 22:30:39 +0900

    2022.01.21

---
+++ baekjoon/src/baekjoon/BinarySearch1.java
@@ -0,0 +1,39 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class BinarySearch1 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuffer sb = new StringBuffer();
+		
+		int n = Integer.parseInt(st.nextToken());
+		Set<Integer> hs = new HashSet<>();
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			hs.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		int m = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<m; i++) {
+			int input = Integer.parseInt(st.nextToken());
+			if(hs.contains(input)) {
+				sb.append(1).append("\n");
+			}else {
+				sb.append(0).append("\n");
+			}
+		}
+		System.out.println(sb);
+	}
+
+}
