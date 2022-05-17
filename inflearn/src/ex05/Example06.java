e7dc962b858c75663dcc1e93fbda5883f0fd0fa9
doongjun <doongjun.kim@gmail.com>
2022-05-17 22:18:04 +0900

    2022-05-17

---
+++ inflearn/src/ex05/Example06.java
@@ -0,0 +1,41 @@
+package ex05;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+// stack
+// 6.공주 구하기
+public class Example06 {
+	static int solution(int n, int k) {
+		int answer =0;
+		
+		Queue<Integer> queue = new LinkedList<>();
+		for(int i=1; i<=n; i++) {
+			queue.offer(i);
+		}
+		
+		int cnt = 1;
+		while(queue.size() > 1) {
+			if(cnt < k) {
+				queue.offer(queue.poll());
+				cnt++;
+			} else {
+				queue.poll();
+				cnt = 1;
+			}
+		}
+		
+		answer = queue.peek();
+		
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		int k = scan.nextInt();
+		System.out.println(solution(n, k));
+	}
+}
