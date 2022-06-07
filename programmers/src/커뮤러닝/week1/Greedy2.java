1b8997811b369282f03d80386645cd4f36c4ee74
doongjun <doongjun@kakao.com>
2022-06-07 21:40:35 +0900

    2022-06-07

--- programmers/src/커뮤러닝/week1/Greedy2.java
+++ programmers/src/커뮤러닝/week1/Greedy2.java
@@ -1,22 +1,18 @@
 package 커뮤러닝.week1;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 // 기지국 설치
-public class Greedy1 {
+public class Greedy2 {
 	public static int solution(int n, int[] stations, int w) {
 		int answer = 0;
-		Queue<Integer> sq = new LinkedList<>();
-		for(int s : stations) sq.offer(s);
+		int stationsIdx = 0;
 		
 		int position = 1;
 		while(position <= n) {
-			if(!sq.isEmpty() && sq.peek()-w <= position) { //맨 왼쪽 <= 현재 포지션
-				position = sq.poll()+w+1; // 맨 오른쪽+1
+			if(stationsIdx < stations.length && stations[stationsIdx]-w <= position) {
+				position = stations[stationsIdx++]+w+1;
 			} else {
 				answer++;
-				position += w*2+1; 
+				position += w*2+1;
 			}
 		}
 		
